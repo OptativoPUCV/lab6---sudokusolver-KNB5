1af445e912b6f7bc4a1fdb041a15f77bc08c5e6f
Your Name <you@example.com>
2024-05-16 06:38:17 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -72,10 +72,10 @@ int is_valid(Node* n) {
             int numSM[10] = {0};
             for (int f = j; f < j + 3; f++) {
                 for (int c = k; c < k + 3; c++) {
-                    if (n->sudo[f][c] != 0 && numSM[n->sudo[f][c]] == 1) {
-                        return 0;
+                    if (n->sudo[f][c] != 0 && numSM[n->sudo[f][c]] == 0) {
+                       numSM[n->sudo[f][c]] = 1;
                     } else {
-                        numSM[n->sudo[f][c]] = 1;
+                        return 0;
                     }
                 }
             }
