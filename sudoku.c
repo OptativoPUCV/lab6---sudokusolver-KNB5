78bc5ca7736f576150776a1591d87ba597cee2f7
Your Name <you@example.com>
2024-05-16 05:47:58 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -52,13 +52,13 @@ int is_valid(Node* n) {
       int numF[10] = {0};
       for(k = 0; k < 9; k++)
       {
-         if(n->sudo[j][k] != 0 && numF[n->sudo[j][k]] == 1)
+         if(n->sudo[j][k] != 0 && numF[n->sudo[j][k]] == 0)
          {
-            return 0;
+            numF[n->sudo[j][k]] = 1;
          }
          else
          {
-            numF[n->sudo[j][k]] = 1;
+            return 0;
          }
       }
    }
@@ -68,13 +68,13 @@ int is_valid(Node* n) {
       int numC[10] = {0};
       for(j = 0; j < 9; j++)
       {
-         if(n->sudo[j][k] != 0 && numC[n->sudo[j][k]] == 1)
+         if(n->sudo[j][k] != 0 && numC[n->sudo[j][k]] == 0)
          {
-            return 0;
+            numC[n->sudo[j][k]] = 1;
          }
          else
          {
-            numC[n->sudo[j][k] == 0] = 1;
+            return 0;
          }
       }
    }
@@ -88,13 +88,13 @@ int is_valid(Node* n) {
          {
             for(c = k; c < k + 3; c++)
             {
-               if(n->sudo[f][c] != 0 && numSM[n->sudo[f][c]] == 1)
+               if(n->sudo[f][c] != 0 && numSM[n->sudo[f][c]] == 0)
                {
-                  return 0;
+                  numSM[n->sudo[f][c]] = 1;
                }
                else
                {
-                  numSM[n->sudo[f][c]] = 1;
+                  return 0;
                }
             }
          }
